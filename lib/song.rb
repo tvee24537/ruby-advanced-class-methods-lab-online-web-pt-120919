971f7a522082700e78a98810143961229731f43e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-16 23:21:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,14 @@ class Song  #given
     result
   end
   
-  
+  def self.find_or_create_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
+    end
+  end
   
 end
