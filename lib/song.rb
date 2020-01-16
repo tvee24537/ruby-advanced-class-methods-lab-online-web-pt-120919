07a5aed2cc55c6e60e3d997c336caef9ebc1aa29
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-16 23:19:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,11 +24,11 @@ class Song  #given
   end
   
   def self.create_by_name(name)
-    song = self.new 
+    song = self.create
     song.name = name
-    @@all << song
     song
   end
+  
   
   
 end
